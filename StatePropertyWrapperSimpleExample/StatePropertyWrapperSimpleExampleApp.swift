757d70bb495008a2c76f5cfb773d814bebacff4e
Abdul <AMalik@lcs.on.ca>
2021-11-29 11:23:14 -0500

    Initial Commit

---
+++ StatePropertyWrapperSimpleExample/StatePropertyWrapperSimpleExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  StatePropertyWrapperSimpleExampleApp.swift
+//  StatePropertyWrapperSimpleExample
+//
+//  Created by Abdul Malik on 2021-11-29.
+//
+
+import SwiftUI
+
+@main
+struct StatePropertyWrapperSimpleExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
